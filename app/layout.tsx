0bdedebb0d97d90606daa7bc2c9018fc6388631a
Alan Parroquin <alan8pf@gmail.com>
2026-03-20 10:19:39 -0600

    layout 3.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -107,19 +107,30 @@ export default function RootLayout({
   return (
     <html lang="es" className={inter.variable}>
       <head>
-                  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                    })(window,document,'script','dataLayer','GTM-TF5BZSGC');
-                  </script>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');
+            `,
+          }}
+        />
       </head>
 
       <body className={`${inter.className} min-h-screen bg-white text-gray-900`}>
-       <noscript>
-              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TF5BZSGC"
-               height="0" width="0" style="display:none;visibility:hidden"></iframe>
-      </noscript>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
 
         <script
           type="application/ld+json"
